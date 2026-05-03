fc36ce2348ee24df9e62491cd2f3b38d6baf8d7e
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-05-03 17:59:38 +0300

    Creating Tag model

---
+++ backend/Models/Tag.cs
@@ -0,0 +1,8 @@
+namespace MaarivMiniApp.Api.Models;
+
+public class Tag
+{
+    public int TagId { get; set; }
+    public string TagName { get; set; } = string.Empty;
+    public string TagImage { get; set; } = string.Empty;
+}
