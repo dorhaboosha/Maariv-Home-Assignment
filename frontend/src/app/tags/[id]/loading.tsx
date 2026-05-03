cad5224e96971630740f66b68f62253789deac41
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-05-03 20:15:45 +0300

    Creating laoding page for tag page

---
+++ frontend/src/app/tags/[id]/loading.tsx
@@ -0,0 +1,8 @@
+export default function TagLoading() {
+  return (
+    <div className="page-container flex flex-col gap-4 animate-pulse">
+      <div className="h-7 bg-gray-200 rounded w-1/3" />
+      <div className="h-48 bg-gray-200 rounded w-full" />
+    </div>
+  );
+}
